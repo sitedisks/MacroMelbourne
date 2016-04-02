571505ef400901e28bf33ac1e384ebe6c0ebe9fb
Peter <peter@sitedisks.com.au>
2016-04-02 23:07:30 +1100

    images add

---
+++ mm.Storm/app/macroApp.js
@@ -0,0 +1,1 @@
+﻿angular.module('macroApp', ['ui.bootstrap']);
---
+++ mm.Storm/app/macroController.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('macroApp').controller('macroAppCtrl', ['$scope', '$uibModal',
+        function ($scope, $uibModal) {
+            
+        }]);
+
+})();
